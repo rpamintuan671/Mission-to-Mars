b7abfb4f32428e085f8fea32b579e46921c8e5b1
rpamintuan671 <106283411+rpamintuan671@users.noreply.github.com>
2022-09-02 11:29:39 -0700

    app.py completed

--- app.py
+++ app.py
@@ -13,14 +13,12 @@ def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
-   @app.route("/scrape")
+@app.route("/scrape")  
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return redirect('/', code=302)
-
-   .update_one(query_parameter, {"$set": data}, options)
-
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    return redirect ('/', code=302)   
+  
 if __name__ == "__main__":
    app.run()
